0f5986a00a84470318d0e672f947a781c2500b34
Joseph Salisbury <salisbury.joseph@gmail.com>
2016-07-09 12:23:32 +0100

    Adds comment to RootCmd

--- cmd/prometheus-pingdom-exporter.go
+++ cmd/prometheus-pingdom-exporter.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// RootCmd is the main Cobra Command.
 var RootCmd = &cobra.Command{
 	Use:   "prometheus-pingdom-exporter",
 	Short: "prometheus-pingdom-exporter exports Pingdom metrics to Prometheus",
